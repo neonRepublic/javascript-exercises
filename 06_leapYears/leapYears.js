da48d7bdd167550b32fd3f7d183ba11361efacdc
neonRepublic <194363997+neonRepublic@users.noreply.github.com>
2025-02-07 19:29:04 -0500

    updated js 2725 1929 *practice*

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
-};
+const leapYears = function (theYear) {
+    const isYearDivisibleByFour = theYear % 4 === 0;
+    const isCentury = theYear % 100 === 0;
+    const isYearDivisibleByFourHundred = theYear % 400 === 0;
+  
+    if (
+      isYearDivisibleByFour &&
+      (!isCentury || isYearDivisibleByFourHundred)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
 // Do not edit below this line
 module.exports = leapYears;
