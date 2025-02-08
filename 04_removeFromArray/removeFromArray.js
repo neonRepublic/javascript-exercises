da425d714582ff317363d4bbe5f6e28fb86eb412
neonRepublic <194363997+neonRepublic@users.noreply.github.com>
2025-02-07 19:08:01 -0500

    updated js 2725 1908

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (num, ...args) {
+    
+    const myArray = [];
+    
+    num.forEach((item) => {
+
+        if (!args.includes(item)) {
+            myArray.push(item);
+          }
+        });
+
+        return myArray;
+
+    
 };
 
 // Do not edit below this line
